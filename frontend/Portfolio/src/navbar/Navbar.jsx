20d0f8d9d02475ba91cdc9558956ba8fde843767
Ismael <ismaeltorresan1@gmail.com>
2023-03-08 11:30:25 -0300

    scss

--- frontend/Portfolio/src/navbar/Navbar.jsx
+++ frontend/Portfolio/src/navbar/Navbar.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="navbar">
+      <div className="navbar__logo">
+        <img src="image.logo" alt="logo" />
+      </div>
+      <ul className="navbar__links">
+        {["home", "about", "work", "skills", "contact"].map((i) => (
+          <li key={i}>
+            <a href="{i}">{i}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
