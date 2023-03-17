a9741923681bf88ec3c032550beb378f5cb341c7
Ismael <ismaeltorresan1@gmail.com>
2023-03-17 10:20:01 -0300

    clean comments

--- frontend/Portfolio/src/App.js
+++ frontend/Portfolio/src/App.js
@@ -1,24 +1,3 @@
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import Dashboard from "./pages/Dashboard";
-
-// function PageA() {
-//   return <h1>Page a</h1>;
-// }
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         <Route path="/" element={<Dashboard />}>
-//           <Route path="a" element={<PageA />} />
-//         </Route>
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import "./App.scss";
 
